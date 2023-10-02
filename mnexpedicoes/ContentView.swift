d0fd32366f634edd99c7f38b9fdfcd0c508a635a
cbentoadm <cesar.bento@ceptis.com.br>
2023-10-02 14:16:29 -0300

    Initial Commit

---
+++ mnexpedicoes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mnexpedicoes
+//
+//  Created by Bento, Cesar - Admin on 02/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mnexpedicoes/mnexpedicoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  mnexpedicoesApp.swift
+//  mnexpedicoes
+//
+//  Created by Bento, Cesar - Admin on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct mnexpedicoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
